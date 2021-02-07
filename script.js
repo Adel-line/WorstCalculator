29915b3a34abd9c7ca41de3991c4b8c9613a46a6
Adelina <adelina.sahlean@gmail.com>
2021-02-07 22:16:17 +0100

    finish touches

--- script.js
+++ script.js
@@ -1,49 +1,27 @@
 "use strict";
+
 init();
 
 function init() {
-  //Init calc + Button
-  document.querySelector("#calculate").addEventListener("click", start);
-  console.log("start calculator");
+  document.getElementById("calculate").addEventListener("click", start);
 }
 
 function start() {
-  // Getting Elements
-  let firstchoice = document.querySelector("#firstnumber");
-  let secondchoice = document.querySelector("#secondnumber");
-  let operator = document.querySelector("#operator");
+    console.log("Start");
+  let firstchoice = document.getElementById("firstnumber").value;
+  let secondchoice = document.getElementById("secondnumber").value;
+  const operation = document.getElementById("operator").value;
   let no1 = Number(firstchoice);
   let no2 = Number(secondchoice);
-  calculator();
-}
 
-// Calculator operations
-function calculator() {
-  console.log("Calculator");
-  if (operator === "add") {
-    let li = document.createElement("li");
-    document.getElementById("#results").innerHTML = no1 + no2;
-    res(li);
-  } else if (operator === "sub") {
-    let li = document.createElement("li");
-    li.innerHTML = no1 - no2;
-    res(li);
-  } else if (operator === "mul") {
-    let li = document.createElement("li");
-    li.innerHTML = no1 * no2;
-    res(li);
-  } else if (operator === "div") {
-    let li = document.createElement("li");
-    li.innerHTML = no1 / no2;
-    res(li);
+  if (operation === "add") {
+   document.getElementById("results").innerHTML = no1 + no2;
+  } else if (operation === "sub") {
+   document.getElementById("results1").innerHTML = no1 - no2;
+  } else if (operation === "mul") {
+    document.getElementById("results2").innerHTML = no1 * no2;
+  } else if (operation === "div") {
+     document.getElementById("results3").innerHTML = no1 / no2;
   }
 }
 
-function res(li) {
-  console.log(li);
-  console.log("hio this checked");
-  document.querySelector("#results").appendChild(li);
-
-  console.log("hio this not checked");
-  document.querySelector("#results").appendChild(li);
-}
